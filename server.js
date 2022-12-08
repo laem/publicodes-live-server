7c6f95cf4bb5c98692dfaced9dfa2397b7a8e538
Mael <mael.thomas@data.gouv.fr>
2022-12-08 18:45:14 +0100

    PORT in MAJ

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ yUtils.setPersistence({
   },
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log("listening on port:" + port);
 });
