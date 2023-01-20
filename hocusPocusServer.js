86a36b5ace8b3330e17c5af28d7d7df4870b4fb8
Mael <mael.thomas@data.gouv.fr>
2023-01-20 15:22:41 +0100

    Ajout d'extensions

--- hocusPocusServer.js
+++ hocusPocusServer.js
@@ -1,9 +1,13 @@
 import { Server } from "@hocuspocus/server";
+import { Logger } from "@hocuspocus/extension-logger";
+import { SQLite } from "@hocuspocus/extension-sqlite";
 
 const port = process.env.PORT || 3000;
 
 const server = Server.configure({
   port,
+  address: "localhost",
+  extensions: [new Logger(), new SQLite()],
 });
 
 server.listen();
